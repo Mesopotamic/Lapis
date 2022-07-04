b23c86cde649f74ef1d3a56e66375fee23429f68
Lawrence <Lawrence.martin.green@gmail.com>
2022-07-04 23:56:50 +0100

    Use a better windows entry point
    Spawns a gui application without needing to wrap winmain

--- Source/cmake/meso_cross_platform_executable.cmake
+++ Source/cmake/meso_cross_platform_executable.cmake
@@ -2,15 +2,14 @@
 
 function(meso_add_executable target_name)
 
-	# Depending on the target platform there may need to be a cmake semantic
-	# I.e specifying win32 will change the linker flags to make winmain the entry
-	# point and therefore we won't auto spawn a terminal
+	# We can set cmake linker flags before creating the executable, and they 
+	# will be caried on to the executable. For example on windows we can set
+	# the entry point so that int main can be used without spawning a terminal
 	if(WIN32)
-		set(CMAKE_EXE_SEMANTIC "WIN32")
-	else()
-		set(CMAKE_EXE_SEMANTIC "")
+		set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} 
+			/SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup") 
 	endif()
-
+	
 	# If the user has specified source files then they will be in the extra 
 	# cmake arguments stored in argn
 	add_executable(${target_name} ${CMAKE_EXE_SEMANTIC} ${ARGN})
